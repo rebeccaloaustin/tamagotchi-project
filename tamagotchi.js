6f4882f0fcfc39da7ca52586e0cfd7653d41ee0e
rebeccaloaustin <rebeccaloaustin@gmail.com>
2023-09-26 10:53:33 -0400

    bars created js outline created

---
+++ tamagotchi.js
@@ -0,0 +1,15 @@
+//define the variables
+let hunger = 100;
+let energy = 100;
+let attention = 100;
+let lose = 0;
+
+//function to check end of game
+
+//function to switch out  pictures at end game
+
+//functions to deplete hunger, energy, & attention
+
+//functions to increase hunger, energy, & attention
+
+//onclick make buttons run increase hunger, energy, & attention functions
