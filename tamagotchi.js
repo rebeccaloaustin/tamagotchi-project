708f8649134dcf5c3e29f5383548f32d8a46c6ca
rebeccaloaustin <rebeccaloaustin@gmail.com>
2023-09-29 11:26:22 -0400

    javascript added to progress bars

--- tamagotchi.js
+++ tamagotchi.js
@@ -1,15 +1,55 @@
 //define the variables
+window.onload = function() {
 let hunger = 100;
 let energy = 100;
 let attention = 100;
-let lose = 0;
+let clock;
 
-//function to check end of game
+//reference to progress bars
+const hungryBar = document.getElementById("hungryBar");
+const energyBar = document.getElementById("sleepyBar");
+const attentionBar = document.getElementById("loveyBar");
 
-//function to switch out  pictures at end game
+//function to update the progress bars
+function updateBars() {
+    hungryBar.value = hunger;
+    energyBar.value = energy;
+    attentionBar.value = attention;
+}
 
-//functions to deplete hunger, energy, & attention
-
-//functions to increase hunger, energy, & attention
-
-//onclick make buttons run increase hunger, energy, & attention functions
+//function to deplete the bars
+function depleteStats() {
+    if (hunger > 0) hunger -= 1;
+    if (energy > 0) energy -= 1;
+    if (attention > 0) attention -= 1;
+    updateBars();
+//checking for end of game
+    if (hunger === 0 || energy === 0 || attention === 0) {
+        clearInterval(clock);
+        alert("Your Tamagotchi has passed away. Game over!");
+    }
+}
+//starts the clock to continuously deplete stats
+clock = setInterval(depleteStats, 1000);
+//event listener for the feed button
+document.getElementById("feedbtn").onclick = function () {
+    if (hunger < 100) {
+        hunger += 10;
+        updateBars();
+    }
+};
+//event listener for the sleep button
+document.getElementById("sleepbtn").onclick = function () {
+    if (energy < 100) {
+        energy += 10;
+        updateBars();
+    }
+};
+//event listener for the love button
+document.getElementById("lovebtn").onclick = function () {
+    if (attention < 100) {
+        attention += 10;
+        updateBars();
+    }
+};
+};
