3e8c971295c1d1bab2cdf72d72efc96aa346a01e
rebeccaloaustin <rebeccaloaustin@gmail.com>
2023-10-02 20:51:35 -0400

    refactored my code and created a game object

--- tamagotchi.js
+++ tamagotchi.js
@@ -1,82 +1,100 @@
- 
-//define the variables
-window.onload = function() {
-let hunger = 100;
-let energy = 100;
-let attention = 100;
-let age = 0;
-let clock;
+const game = {
+    petName: null,
+    hunger: 100,
+    energy: 100,
+    attention: 100,
+    age: 0,
+    clock: null,
+    ageInterval: null,
 
-//reference to progress bars
-const hungryBar = document.getElementById("hungryBar");
-const energyBar = document.getElementById("sleepyBar");
-const attentionBar = document.getElementById("loveyBar");
-const ageCounter = document.getElementById("ageDisplay");
-const body = document.body;
+    startGame() {
+        window.onload = () => {
+            // Reference to progress bars
+            this.hungryBar = document.getElementById("hungryBar");
+            this.energyBar = document.getElementById("sleepyBar");
+            this.attentionBar = document.getElementById("loveyBar");
+            this.ageCounter = document.getElementById("ageDisplay");
+            this.body = document.body;
 
-//starts the clock to continuously deplete stats
-const ageInterval = setInterval(increaseAge, 5000);
-clock = setInterval(depleteStats, 800);
+            // Event listeners for buttons
+            document.getElementById("feedbtn").onclick = () => this.feed();
+            document.getElementById("sleepbtn").onclick = () => this.sleep();
+            document.getElementById("lovebtn").onclick = () => this.love();
+            document.getElementById("namebtn").onclick = () => this.nameTamagotchi();
 
-//function to increase age of tamagotchi
-function increaseAge() {
-    age++;
-    ageCounter.textContent = age; 
-    if (age > 5){
-    document.getElementById("walkingDog").style.display = "none";
-    document.getElementById("oldDog").style.display = "block";
-    body.classList.add('old-background');
-}}
+            // Clocks to continuously deplete stats & increase age
+            this.ageInterval = setInterval(() => this.increaseAge(), 5000);
+            this.clock = setInterval(() => this.depleteStats(), 800);
+        };
+    },
 
-//function to update the progress bars
-function updateBars() {
-    hungryBar.value = hunger;
-    energyBar.value = energy;
-    attentionBar.value = attention;
-}
+    // Function to get tamagotchi name
+    nameTamagotchi() {
+        const petName = prompt("tamagotchi name");
+        const nameDisplay = document.getElementById("name");
+        nameDisplay.textContent = `name: ${petName}`;
+        namebtn.style.display = "none";
+    },
 
-//function to deplete the bars
-function depleteStats() {
-    if (hunger > 0) hunger -= 5;
-    if (energy > 0) energy -= 5;
-    if (attention > 0) attention -= 5;
-    updateBars();
+    // Function to increase age of tamagotchi
+    increaseAge() {
+        this.age++;
+        this.ageCounter.textContent = this.age;
+        if (this.age > 5) {
+            document.getElementById("walkingDog").style.display = "none";
+            document.getElementById("oldDog").style.display = "block";
+            this.body.classList.add('old-background');
+        }
+    },
 
-//checking for end of game
-    if (hunger === 0 || energy === 0 || attention === 0) {
-        clearInterval(clock);
-        clearInterval(ageInterval); 
-        document.getElementById("walkingDog").style.display = "none";
-        document.getElementById("ghostDog").style.display = "block";
-        document.getElementById("oldDog").style.display = "none";
-        body.classList.remove('old-background');
-        body.classList.add('ghost-background');
-        document.getElementById("title").textContent = "Your tamagotchi has passed away. Game over!";
+    // Function to update the progress bars
+    updateBars() {
+        this.hungryBar.value = this.hunger;
+        this.energyBar.value = this.energy;
+        this.attentionBar.value = this.attention;
+    },
 
-        // alert("Your Tamagotchi has passed away. Game over!");
-    }
-}
+    // Function to deplete the bars
+    depleteStats() {
+        if (this.hunger > 0) this.hunger -= 5;
+        if (this.energy > 0) this.energy -= 5;
+        if (this.attention > 0) this.attention -= 5;
+        this.updateBars();
 
-//event listener for the feed button
-document.getElementById("feedbtn").onclick = function () {
-    if (hunger < 100) {
-        hunger += 10;
-        updateBars();
+        // Checking for the end of the game
+        if (this.hunger === 0 || this.energy === 0 || this.attention === 0) {
+            clearInterval(this.clock);
+            clearInterval(this.ageInterval);
+            document.getElementById("walkingDog").style.display = "none";
+            document.getElementById("ghostDog").style.display = "block";
+            document.getElementById("oldDog").style.display = "none";
+            this.body.classList.remove('old-background');
+            this.body.classList.add('ghost-background');
+            document.getElementById("title").textContent = "Your tamagotchi has passed away. Game over!";
+        }
+    },
+
+    feed() {
+        if (this.hunger < 100) {
+            this.hunger += 10;
+            this.updateBars();
+        }
+    },
+
+    sleep() {
+        if (this.energy < 100) {
+            this.energy += 10;
+            this.updateBars();
+        }
+    },
+
+    love() {
+        if (this.attention < 100) {
+            this.attention += 10;
+            this.updateBars();
+        }
     }
 };
-//event listener for the sleep button
-document.getElementById("sleepbtn").onclick = function () {
-    if (energy < 100) {
-        energy += 10;
-        updateBars();
-    }
-};
-//event listener for the love button
-document.getElementById("lovebtn").onclick = function () {
-    if (attention < 100) {
-        attention += 10;
-        updateBars();
-    }
-};
 
-};
+// Start the game
+game.startGame();
